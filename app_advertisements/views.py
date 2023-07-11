bf349a6caa0e58eb0dac2c5ecf539e9c08039500
skorohodaa <132@mail.ru>
2023-07-11 22:51:54 +0700

    5 lesson

---
+++ app_advertisements/views.py
@@ -0,0 +1,9 @@
+from django.shortcuts import render
+
+def index(request):
+    return render(request, 'index.html')
+
+def top_sellers(request):
+    return render(request, 'top-sellers.html')
+
+# Create your views here.
